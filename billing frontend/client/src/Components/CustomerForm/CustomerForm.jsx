0cd6f1dd78401aefc0030c33661044e5f3801b4a
pradeepdalvi14 <pradeepd6915@gmail.com>
2026-03-21 16:36:18 +0530

    added customer form from front end

--- billing frontend/client/src/Components/CustomerForm/CustomerForm.jsx
+++ billing frontend/client/src/Components/CustomerForm/CustomerForm.jsx
@@ -1,10 +1,25 @@
 import './CustomerForm.css';
 
-const CustomerForm = () => {
+const CustomerForm = ({customerName, mobileNumber, setCustomerName, setMobileNumber}) => {
 
     return (
-        <div>
-            customer form
+        <div className="p-1">
+            <div className="mb-3">
+                <div className="d-flex align-items-center gap-2">
+                    <label htmlFor="customerName" className="col-4">
+                        Customer Name
+                    </label>
+                    <input type="text" className="form-control" id="customerName"  value={customerName} onChange={(e) => setCustomerName(e.target.value)}/>
+                </div>
+            </div>
+            <div className='mb-3'>
+                <div className="d-flex align-items-center gap-2">
+                    <label htmlFor="mobileNumber" className="col-4">
+                        Mobile Number
+                    </label>
+                    <input type="text" className="form-control" id="mobileNumber"  value={mobileNumber} onChange={(e)=>setMobileNumber(e.target.value) }/>
+                </div>
+            </div>
         </div>
     )
 }
