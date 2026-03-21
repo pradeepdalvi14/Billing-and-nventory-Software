9763cf612a4441ebffe1a1bcfb42e7459fb5cddc
pradeepdalvi14 <pradeepd6915@gmail.com>
2026-03-21 18:02:03 +0530

    cart item component added

--- billing frontend/client/src/Components/Item/Item.jsx
+++ billing frontend/client/src/Components/Item/Item.jsx
@@ -1,6 +1,15 @@
 import './Item.css';
-
+import { useContext } from 'react';
+import { AppContext } from '../../Context/AppContext';
 const Item = ({ itemName, itemPrice, itemImage, itemId }) => {
+    const {addToCart} = useContext(AppContext);
+    const handleAddToCart = () => {
+        addToCart({
+            name: itemName, 
+            price: itemPrice,
+            quantity: 1,
+            itemId: itemId});
+    }
     return (
         <div className="p-3 bg-dark rounded shadow-sm h-100 d-flex align-items-center item-card">
             <div style={{ position: "relative", marginRight: "15px" }}>
@@ -13,7 +22,7 @@ const Item = ({ itemName, itemPrice, itemImage, itemId }) => {
             <div className="d-flex flex-column justify-content-between align-items-cennter ms-3" 
             style={{height:"100%"}}>
                 <i className="bi bi-cart-plus fs-4 text-warning"></i>
-                <button className="btn btn-success btn-sm">
+                <button className="btn btn-success btn-sm" onClick={handleAddToCart}>
                     <i className="bi bi-plus"></i>
                 </button>
 
