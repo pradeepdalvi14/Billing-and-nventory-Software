39c8a7f633de2052988b31f503605227e1f724eb
pradeepdalvi14 <pradeepd6915@gmail.com>
2026-03-28 18:18:23 +0530

    cart item + receipt popup

--- billing frontend/client/src/Components/CartSummary/CartSummary.jsx
+++ billing frontend/client/src/Components/CartSummary/CartSummary.jsx
@@ -1,9 +1,45 @@
+import { useContext } from 'react';
 import './CartSummary.css';
+import { AppContext } from '../../Context/AppContext';
+import ReceiptPopup from '../ReceiptPopup/ReceiptPopup';
+const CartSummary = ({customerName, mobileNumber, setMobileNumber, setCustomerName}) => {
+    
+    const {cartItems} = useContext(AppContext);
+    const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+    const tax = totalAmount * 0.01;
+    const grandTotal = totalAmount + tax;
 
-const CartSummary = () => {
     return (
         <div>
-            cart Summary
+            <div className="mt-2">
+                <div className="cart-summary-details">
+                    <div className="d-flex justify-content-between mb-2">
+                        <span className='text-light'>Items : </span>
+                        <span className='text-light'>₹ {totalAmount.toFixed(2)}</span>
+                    </div>
+                    <div className="d-flex justify-content-between mb-2">
+                        <span className='text-light'>Tax (1%): </span>
+                        <span className='text-light'>₹ {tax.toFixed(2)}</span>
+                    </div>
+                    <div className="d-flex justify-content-between mb-2">
+                        <span className='text-light'>Total </span>
+                        <span className='text-light'>₹ {grandTotal.toFixed(2)}</span>
+                    </div>
+                </div>
+                <div className="d-flex gap-3">
+                    <button className='btn btn-success flex-grow-1'>
+                        Cash
+                    </button>
+                    <button className='btn btn-primary flex-grow-1'>
+                        UPI
+                    </button>
+                </div>
+                <div className='d-flex gap-3 mt-3'>
+                    <button className='btn btn-warning flex-grow-1'>
+                        Place Order
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ billing frontend/client/src/Components/ReceiptPopup/ReceiptPopup.jsx
@@ -0,0 +1,9 @@
+import './ReceiptPopup.css';
+
+const ReceiptPopup = () => {
+    return (
+        <div>ReceiptPopup</div>
+    )
+}
+
+export default ReceiptPopup;
